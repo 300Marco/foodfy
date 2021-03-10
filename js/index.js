2fdb9b4d4e81f535bf592e7289415251a9213f73
Marco Santana <marcos.maio96@gmail.com>
2021-03-10 13:57:16 -0300

    updated index.js, code cleanup

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 const modalOverlay = document.querySelector(".modal-overlay")
 const cards = document.querySelectorAll(".card")
 
-// Pegando classe com informações no modal
-// const modalContent = document.getElementsByClassName("name-dish")
-
-// Selecionando o elemento no modal 
 const modalH3 = document.querySelector("h3")
 const modalSpan = document.querySelector("span")
 
@@ -19,20 +15,15 @@ for(let card of cards) {
         modalOverlay.querySelector("img").src = `../assets/${imgId}`
 
         /**modal info */
+        const cardH4 = card.querySelector("h4").textContent 
+        const cardP = card.querySelector("p").textContent 
 
-        const cardH4 = card.querySelector("h4").textContent // pegando informação
-        const cardP = card.querySelector("p").textContent // pegando informação
-
-        //
         modalH3.innerHTML = cardH4
         modalSpan.innerHTML = cardP
 
         modalContent.appendChild(modalH3)
         modalContent.appendChild(modalSpan)
-
-        // const modalH3 = card.querySelector("h3").textContent
-
-        // modalH3.appendChild()
+        
     })
 
 }
