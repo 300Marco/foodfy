218611cecb5e6319c6bca89feee6b36fc7747104
Marco Santana <marcos.maio96@gmail.com>
2021-03-11 10:44:26 -0300

    responsiveness adjustments

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
+// modal functionality
 const modalOverlay = document.querySelector(".modal-overlay")
 const cards = document.querySelectorAll(".card")
 
+// modal info
 const modalH3 = document.querySelector("h3")
 const modalSpan = document.querySelector("span")
 
@@ -8,13 +10,14 @@ for(let card of cards) {
 
     card.addEventListener("click", function() {
 
+        /* modal functionality*/
         const imgId = card.getAttribute("id")
 
         modalOverlay.classList.add("active")
 
         modalOverlay.querySelector("img").src = `../assets/${imgId}`
 
-        /**modal info */
+        /* modal info */
         const cardH4 = card.querySelector("h4").textContent 
         const cardP = card.querySelector("p").textContent 
 
@@ -28,6 +31,7 @@ for(let card of cards) {
 
 }
 
+// close modal
 document.querySelector(".close-modal").addEventListener("click", function() {
     
     modalOverlay.classList.remove("active")
